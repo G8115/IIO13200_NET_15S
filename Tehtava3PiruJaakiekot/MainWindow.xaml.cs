58ec8caae05109e5fc284aa82d2ab6a4a9c32e7b
G8115 <G8115@student.jamk.fi>
2015-09-10 18:11:45 +0300

    Lisätty businessLogiikka väliin ja muuten trimmattu turhuuksia pois

--- Tehtava3PiruJaakiekot/MainWindow.xaml.cs
+++ Tehtava3PiruJaakiekot/MainWindow.xaml.cs
@@ -21,45 +21,25 @@ namespace Tehtava3PiruJaakiekot
     /// </summary>
     public partial class MainWindow : Window
     {
-        private List<Team> tiimit;
-        private Jaakiekkoilija tempKiekkoilija;
-        private bool uusiPelaaja;
+        private blPiruJaakiekot businessTyhmyys;
+        private bool newPlayer;
         public MainWindow()
         {
             InitializeComponent();
 
-            tempKiekkoilija = new Jaakiekkoilija("", "", "", 1);
+            businessTyhmyys = new blPiruJaakiekot();
 
-            tiimit = new List<Team>();
-            tiimit.Add(new Team("a"));
-            tiimit.Add(new Team("aa"));
-            tiimit.Add(new Team("aaa"));
-            tiimit.Add(new Team("aaaa"));
-            tiimit.ElementAt(0).Players.Add(new Jaakiekkoilija("1", "1", tiimit.ElementAt(0).Name, 1));
-            tiimit.ElementAt(0).Players.Add(new Jaakiekkoilija("2", "2", tiimit.ElementAt(0).Name, 1));
-            tiimit.ElementAt(0).Players.Add(new Jaakiekkoilija("3", "3", tiimit.ElementAt(0).Name, 1));
-            tiimit.ElementAt(0).Players.Add(new Jaakiekkoilija("4", "4", tiimit.ElementAt(0).Name, 1));
-            tiimit.ElementAt(1).Players.Add(new Jaakiekkoilija("5", "5", tiimit.ElementAt(1).Name, 1));
-            tiimit.ElementAt(1).Players.Add(new Jaakiekkoilija("6", "6", tiimit.ElementAt(1).Name, 1));
-            tiimit.ElementAt(1).Players.Add(new Jaakiekkoilija("7", "7", tiimit.ElementAt(1).Name, 1));
-            tiimit.ElementAt(1).Players.Add(new Jaakiekkoilija("8", "8", tiimit.ElementAt(1).Name, 1));
-            tiimit.ElementAt(2).Players.Add(new Jaakiekkoilija("9", "9", tiimit.ElementAt(2).Name, 1));
-            tiimit.ElementAt(2).Players.Add(new Jaakiekkoilija("10", "10", tiimit.ElementAt(2).Name, 1));
-            tiimit.ElementAt(2).Players.Add(new Jaakiekkoilija("11", "11", tiimit.ElementAt(2).Name, 1));
-            tiimit.ElementAt(2).Players.Add(new Jaakiekkoilija("12", "12", tiimit.ElementAt(2).Name, 1));
-            tiimit.ElementAt(3).Players.Add(new Jaakiekkoilija("13", "13", tiimit.ElementAt(3).Name, 1));
-            tiimit.ElementAt(3).Players.Add(new Jaakiekkoilija("14", "14", tiimit.ElementAt(3).Name, 1));
-            tiimit.ElementAt(3).Players.Add(new Jaakiekkoilija("15", "15", tiimit.ElementAt(3).Name, 1));
-            tiimit.ElementAt(3).Players.Add(new Jaakiekkoilija("16", "16", tiimit.ElementAt(3).Name, 1));
-
-            foreach(var a in tiimit)
+            newPlayer = true;
+            List<String> tempTeamNames = new List<string>();
+            tempTeamNames = businessTyhmyys.getTeams();
+            foreach(var a in tempTeamNames)
                 cbTeams.Items.Add(a);
         }
 
         private void btnCreate_Click(object sender, RoutedEventArgs e)
         {
-            uusiPelaaja = true;
-            tempKiekkoilija = new Jaakiekkoilija("", "", "", 1);
+            setMessage("Syötä uuden pelaajan tiedot ja paina tallenna napiketta");
+            newPlayer = true;
             txtBoxFirstName.Text = "";
             txtBoxLastName.Text = "";
             txtBoxPrice.Text = "";
@@ -67,46 +47,54 @@ namespace Tehtava3PiruJaakiekot
 
         private void btnSave_Click(object sender, RoutedEventArgs e)
         {
- 
+
             int temp = getPrice();
             if (temp != -1)
             {
-                if (!uusiPelaaja)
-                    foreach (var t in tiimit)
-                        t.removePlayer(tempKiekkoilija.FirstName);
-
-                if (checkName(txtBoxFirstName.Text, txtBoxLastName.Text))
+                if (newPlayer)
                 {
-                    tempKiekkoilija.FirstName = txtBoxFirstName.Text;
-                    tempKiekkoilija.LastName = txtBoxLastName.Text;
-                    tempKiekkoilija.Price = temp;
-                    tempKiekkoilija.team = cbTeams.SelectedItem.ToString();
-
-                    tiimit.Find(r => r.Name == cbTeams.SelectedItem.ToString()).Players.Add(tempKiekkoilija);
+                    if (businessTyhmyys.addPlayer(txtBoxFirstName.Text, txtBoxLastName.Text, cbTeams.SelectedItem.ToString(), temp))
+                    {
+                        setMessage("Uusi pelaaja lisätty onnistuneesti");
+                    }
+                    else
+                    {
+                        setMessage("Nimi on jo olemassa");
+                    }
                 }
                 else
                 {
-                    setMessage("Nimi on jo olemassa");
+                    if (businessTyhmyys.savePlayer((Jaakiekkoilija)lbTeamPlayers.SelectedItem, txtBoxFirstName.Text, txtBoxLastName.Text, cbTeams.SelectedItem.ToString(), temp))
+                    {
+                        setMessage("Tiedot tallennettu");
+                    }
+                    else
+                    {
+                        setMessage("Nimi on jo olemassa");
+                    }
                 }
             }
         }
 
         private void btnRemove_Click(object sender, RoutedEventArgs e)
         {
-            ((Team)cbTeams.SelectedItem).removePlayer(tempKiekkoilija.FirstName);
+            setMessage("pelaaja poistettu onnistuneesti");
+            businessTyhmyys.removePlayer(txtBoxFirstName.Text);
         }
 
         private void btnLoadPlayers_Click(object sender, RoutedEventArgs e)
         {
-            lbTeamPlayers.SelectedIndex=-1;
+            setMessage("tiedot ladattu");
             lbTeamPlayers.Items.Clear();
-            Team temp = (Team)cbTeams.SelectedItem;
-            foreach (var o in temp.Players)
-                lbTeamPlayers.Items.Add(o);
+            List<Jaakiekkoilija> tempJaakiekkoList= new List<Jaakiekkoilija>();
+            tempJaakiekkoList = businessTyhmyys.getPlayers(cbTeams.SelectedItem.ToString());
+            foreach (var t in tempJaakiekkoList)
+                lbTeamPlayers.Items.Add(t);
         }
 
         private void btnEnd_Click(object sender, RoutedEventArgs e)
         {
+            setMessage("hei hei");
             this.Close();
         }
 
@@ -132,30 +120,15 @@ namespace Tehtava3PiruJaakiekot
 
         private void lbTeamPlayers_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
-           
-                uusiPelaaja = false;
-                tempKiekkoilija =(Jaakiekkoilija) ((ListBox)sender).SelectedItem;
-            if (lbTeamPlayers.SelectedIndex>=0)
+            setMessage("");
+            newPlayer = false;
+            Jaakiekkoilija tempKiekkoilija = (Jaakiekkoilija)((ListBox)sender).SelectedItem;
+            if (lbTeamPlayers.SelectedIndex >= 0)
             {
                 txtBoxFirstName.Text = tempKiekkoilija.FirstName;
                 txtBoxLastName.Text = tempKiekkoilija.LastName;
                 txtBoxPrice.Text = tempKiekkoilija.Price.ToString();
             }
-        }
-        private bool checkName(String firstName,String lastName)
-        {
-            foreach (var m in tiimit)
-            {
-                foreach (var n in m.Players)
-                {
-                    if (n.FirstName.Equals(txtBoxFirstName.Text, StringComparison.Ordinal) 
-                        || n.LastName.Equals(txtBoxLastName.Text, StringComparison.Ordinal))
-                    {
-                        return false;
-                    }
-                }
-            }
-            return true;
         }
     }
 }
---
+++ Tehtava3PiruJaakiekot/blPiruJaakiekot.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tehtava3PiruJaakiekot
+{
+    class blPiruJaakiekot
+    {
+        private List<Jaakiekkoilija> Pelaajat;
+        public blPiruJaakiekot()
+        {
+              Pelaajat = new List<Jaakiekkoilija>();
+              Pelaajat.Add(new Jaakiekkoilija("1", "1","a", 1));
+              Pelaajat.Add(new Jaakiekkoilija("2", "2","a" , 1));
+              Pelaajat.Add(new Jaakiekkoilija("3", "3", "a" , 1));
+              Pelaajat.Add(new Jaakiekkoilija("4", "4", "a" , 1));
+              Pelaajat.Add(new Jaakiekkoilija("5", "5", "aa" , 1));
+              Pelaajat.Add(new Jaakiekkoilija("6", "6", "aa" , 1));
+              Pelaajat.Add(new Jaakiekkoilija("7", "7", "aa" , 1));
+              Pelaajat.Add(new Jaakiekkoilija("8", "8", "aa" , 1));
+              Pelaajat.Add(new Jaakiekkoilija("9", "9", "aaa" , 1));
+              Pelaajat.Add(new Jaakiekkoilija("10", "10", "aaa" , 1));
+              Pelaajat.Add(new Jaakiekkoilija("11", "11", "aaa" , 1));
+              Pelaajat.Add(new Jaakiekkoilija("12", "12", "aaa" , 1));
+              Pelaajat.Add(new Jaakiekkoilija("13", "13", "aaaa" , 1));
+              Pelaajat.Add(new Jaakiekkoilija("14", "14", "aaaa" , 1));
+              Pelaajat.Add(new Jaakiekkoilija("15", "15", "aaaa" , 1));
+              Pelaajat.Add(new Jaakiekkoilija("16", "16", "aaaa" , 1));
+
+        }
+        private bool checkName(String fName,String lName)
+        {
+            foreach(var p in Pelaajat)
+            {
+                if (p.FirstName.Equals(fName)||p.LastName.Equals(lName))
+                    return false;
+            }
+            return true;
+        }
+        private bool checkOthersNames(Jaakiekkoilija j, String fName, String lName)
+        {
+            foreach (var p in Pelaajat)
+            {
+                if ((p.FirstName.Equals(fName) || p.LastName.Equals(lName))&&!p.Equals(j))
+                    return false;
+            }
+            return true;
+        }
+        public void removePlayer(String nimi)
+        {
+            for(int i = 0; i < Pelaajat.Count; i++)
+            {
+                if (Pelaajat.ElementAt(i).FirstName.Equals(nimi))
+                {
+                    Pelaajat.RemoveAt(i);
+                }
+            }
+        }
+        public bool savePlayer(Jaakiekkoilija pelaaja, String fName, String lName, String team, int price)
+        {
+            if (checkOthersNames(pelaaja,fName, lName))
+            {
+                pelaaja.FirstName = fName;
+                pelaaja.LastName = lName;
+                pelaaja.team = team;
+                pelaaja.Price = price;
+                return true;
+            }
+            return false;
+        }
+        public List<String> getTeams()
+        {
+            List<String> temp = new List<string>();
+            foreach( var p in Pelaajat)
+            {
+                if (!temp.Contains(p.team))
+                {
+                    temp.Add(p.team);
+                }
+            } 
+            return temp;
+        }
+        public bool addPlayer(String fName, String lName, String team, int price)
+        {
+            if (checkName(fName, lName))
+            {
+                Pelaajat.Add(new Jaakiekkoilija(fName, lName, team, price));
+                return true;
+            }
+            return false;
+        }
+        public List<Jaakiekkoilija> getPlayers(String team)
+        {
+            List<Jaakiekkoilija> tempList = new List<Jaakiekkoilija>();
+            foreach(var p in Pelaajat)
+            {
+                if (p.team.Equals(team))
+                {
+                    tempList.Add(p);
+                }
+            }
+            return tempList;
+        }
+    }
+}
