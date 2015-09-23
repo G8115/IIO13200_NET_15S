477962c501ee1f65a0c1d4c0f3f4e434139c8de0
G8115 <G8115@student.jamk.fi>
2015-09-23 18:30:53 +0300

    tehtavaa 5 niin paljon kuin yleensä pytyi ilman sitä dbConnectionString:ä tekemään

---
+++ Tehtava5Lasnaolo/Student.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+//temporay class done without knowing the db structure
+namespace Tehtava5Lasnaolo
+{
+    class Student
+    {
+        public String asioid { get; set; }
+        public String lastname { get; set; }
+        public String firstname { get; set; }
+        public DateTime date { get; set; }
+        public Student()
+        {
+            asioid = "";
+            lastname = "";
+            firstname = "";
+            date = new DateTime();
+        }
+    }
+}
---
+++ Tehtava5Lasnaolo/blDbConnection.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Configuration;
+
+namespace Tehtava5Lasnaolo
+{
+    class blDbConnection
+    {
+        public string query;
+
+        public blDbConnection()
+        {
+            query = ConfigurationManager.AppSettings["dbConnection"];
+        }
+        public DataTable dbSearch(String connectionString)
+        {
+            DataTable temp = new DataTable();
+            // Open connection
+            using (SqlConnection c = new SqlConnection(connectionString))
+            {
+                c.Open();
+                // Create new DataAdapter
+                using (SqlDataAdapter a = new SqlDataAdapter(query, c))
+                {
+                    // Use DataAdapter to fill DataTable
+                    a.Fill(temp);
+                }
+            }
+            return temp;
+        }
+    }
+}
