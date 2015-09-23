9c98a17c98e45f1d4e6783697a7f297463930fc6
G8115 <G8115@student.jamk.fi>
2015-09-23 19:14:04 +0300

    Tehtavasta 6 tehty niin paljon kuin mitä tehtavananto antaa yleensä tehdä Readme.txt tiedostossa lisävalituksia

---
+++ Tehtava6/MainWindow.xaml.cs
@@ -0,0 +1,67 @@
+﻿//Koodannut ja testannut toimivaksi 6.3.2014 EsaSalmik
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace JAMK.ICT.ADOBlanco
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        String viesti;
+        public MainWindow()
+        {
+            InitializeComponent();
+            IniMyStuff();
+        }
+
+        private void IniMyStuff()
+        {
+            //TODO täytetään combobox asiakkaitten maitten nimillä // ei pysty koska mitään ei tietokannasta kerrota
+            //esimerkki kuinka App.Configissa oleva connectionstring luetaan
+            lbMessages.Content = JAMK.ICT.Properties.Settings.Default.Tietokanta;
+
+        }
+
+        private void btnGet3_Click(object sender, RoutedEventArgs e)
+        {
+            //TODO
+            dgCustomers.ItemsSource = DBPlacebo.GetTestCustomers().AsDataView();
+        }
+
+        private void btnGetAll_Click(object sender, RoutedEventArgs e)
+        {
+            //TODO
+            // ei voi hakea tietoa tietokannasta jota ei tunne
+            dgCustomers.ItemsSource = DBPlacebo.GetAllCustomersFromSQLServer("","",out viesti).AsDataView();
+        }
+
+        private void btnGetFrom_Click(object sender, RoutedEventArgs e)
+        {
+            //TODO
+            //hakutermiin tulisi lisätä comboboxin maa mutta koska ei mitään hajua databasesta ei voi mitään
+            dgCustomers.ItemsSource = DBPlacebo.GetAllCustomersFromSQLServer("", "", out viesti).AsDataView();
+        }
+
+        private void btnYield_Click(object sender, RoutedEventArgs e)
+        {
+            //opettaja ei missään kerro mitään jsonista
+            JAMK.ICT.JSON.JSONPlacebo2015 roskaa = new JAMK.ICT.JSON.JSONPlacebo2015();
+            MessageBox.Show(roskaa.Yield());
+        }
+    }
+}
---
+++ Tehtava6/ReadMe.txt
@@ -0,0 +1,12 @@
+﻿Hae GitHubista malliprojekti pohjaksi, ja toteuta tarvittava toiminnallisuus UI:hin että DBPlaceco-luokkaan.
+ei kerro yhtään mitään siitä mitä työssä pitäisi oikeastaan tehdä.
+
+//TODO hae opettajan kertomasta palvelusta JSONia
+jos opettaja ei kerro on vaikea hakea
+
+sekä readme.md ja kommentit.txt eivät sisällä mitää hyödyllistä informaatiota
+
+olisi hyvä jos jossain kerrottaisiin tietokannan muodosta jotain
+esimerkiksi minkänimisestä taulusta oltaisiin tietoa etsimässä,
+ehkäpä jopa semmoinen kaaviokuva tietokannan rakenteesta.
+
