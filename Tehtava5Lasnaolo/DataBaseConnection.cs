bd55311cae1af57d0350253d632430a611d09c82
G8115 <G8115@student.jamk.fi>
2015-09-25 10:49:32 +0300

    Jos jo nyt toimis

---
+++ Tehtava5Lasnaolo/DataBaseConnection.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tehtava5Lasnaolo
+{
+    public static class DataBaseConnection
+    {
+        public static DataTable GetAll()
+        {
+            // basic principle: connect - execute query - disconnect
+            try
+            {
+                String connStr = System.Configuration.ConfigurationManager.ConnectionStrings["Lasnaolot"].ConnectionString; ;
+                SqlConnection myConn = new SqlConnection(connStr);
+                myConn.Open();
+                SqlCommand cmd = new SqlCommand("SELECT asioid,lastname,firstname,date FROM lasnaolot", myConn);
+                DataTable dt = new DataTable();
+                dt.Load(cmd.ExecuteReader());
+                myConn.Close();
+                return dt;
+            }
+            catch (Exception ex)
+            {
+                return null;
+            }
+        }
+        /*Turha sillä on parempi käsitellä Datatablea erikseen
+        public static DataTable GetMy(String name)
+        {
+            // basic principle: connect - execute query - disconnect
+            try
+            {
+                String connStr = System.Configuration.ConfigurationManager.ConnectionStrings["Lasnaolot"].ConnectionString; ;
+                SqlConnection myConn = new SqlConnection(connStr);
+                myConn.Open();
+                SqlCommand cmd = new SqlCommand("SELECT * FROM lasnaolot WHERE ", myConn);
+                DataTable dt = new DataTable();
+                dt.Load(cmd.ExecuteReader());
+                myConn.Close();
+                return dt;
+            }
+            catch (Exception ex)
+            {
+            //kunhan on jokin muutos luokassa jonka pitäisi mennä puhiin mukaan
+                return null;
+            }
+        }
+        */
+    }
+}
---
+++ Tehtava6/blTehtava6.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using JAMK.ICT.ADOBlanco;
+
+namespace JAMK.ICT
+{
+    class blTehtava6
+    {
+        private String viesti = "";
+        private DataTable dt;
+        public blTehtava6()
+        {
+            dt = new DataTable();
+            dt = DBPlacebo.GetAllCustomersFromSQLServer(JAMK.ICT.Properties.Settings.Default.Tietokanta, "Viini", out viesti);
+        }
+        public DataTable GetAll()
+        {
+            return dt;
+        }
+        public List<String> GetCities()
+        {
+            List<String> tempString = new List<String>();
+            foreach (DataRow row in dt.Rows)
+            {
+                tempString.Add(row["City"]);
+            }
+            return tempString;
+        }
+    }
+}
